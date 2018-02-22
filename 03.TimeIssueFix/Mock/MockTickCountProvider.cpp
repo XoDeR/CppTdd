fcb459dadbe56b2aeb290f04f56e2bca81be4dc9
XoDeR <XoDeR,vs@gmail.com>
2018-02-22 21:37:14 +0100

    modified mock with TickCountProvider
    modified mock with TickCountProvider

---
+++ 03.TimeIssueFix/Mock/MockTickCountProvider.cpp
@@ -0,0 +1,42 @@
+#include "MockTickCountProvider.h"
+
+#include "../Tools/Utils.h"
+
+using AmstelTech::Win32::ToString;
+
+namespace AmstelTech 
+{
+	
+namespace Win32 
+{
+	
+namespace Mock 
+{
+
+///////////////////////////////////////////////////////////////////////////////
+// CMockTickCountProvider
+///////////////////////////////////////////////////////////////////////////////
+
+CMockTickCountProvider::CMockTickCountProvider()
+   :  m_tickCount(0)
+{
+
+}
+
+void CMockTickCountProvider::SetTickCount(
+   const DWORD tickCount)
+{
+   ::InterlockedExchange(reinterpret_cast<volatile long *>(&m_tickCount), tickCount);
+}
+
+DWORD CMockTickCountProvider::GetTickCount() const
+{
+   LogMessage(_T("GetTickCount: ") + ToString(m_tickCount));
+
+   return m_tickCount;
+}
+
+} // End of namespace Mock
+} // End of namespace Win32
+} // End of namespace AmstelTech 
+
